766ffa4d7f9aaf0a0035314742ced794f3d20b45
olto <olto@gmail.com>
2022-08-02 17:48:27 +0300

    develop-server

---
+++ server/route/auth.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { register, login, checkUser } from "../controller/auth.js";
+import { checkAuth } from "../utils/checkAuth.js";
+const router = new Router();
+
+// Register
+// http://localhost:3002/api/auth/register
+router.post("/register", register);
+
+// Login
+// http://localhost:3002/api/auth/login
+router.post("/login", login);
+
+// Get Me
+// http://localhost:3002/api/auth/me
+router.get("/check", checkAuth, checkUser);
+
+export default router;
